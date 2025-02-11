c637fb0cc853f1594346445c08fcb286080c31d1
Ridyl <joey.dalatri214@gmail.com>
2025-02-11 11:49:30 -0500

    Added propper CORS variables to docker config as well as to the .env file. Successful read from back end server.

--- directus/directus.js
+++ directus/directus.js
@@ -2,6 +2,6 @@ import { createDirectus, staticToken, rest } from '@directus/sdk';
 
 const client = createDirectus(import.meta.env.VITE_DIRECTUS_URL)
 	.with(staticToken(import.meta.env.VITE_DIRECTUS_ACCESS_TOKEN))
-	.with(rest());
+	.with(rest({ credentials: 'include' }));
 
 export default client;
